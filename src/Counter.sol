fdfc9e7b17d9c3f09279dc816915eabb4574d63e
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-25 23:53:56 +0530

    slight fix

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,20 +1,12 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Counter { 
-  uint public nums;
-  
-  constructor(uint _nums){
-    nums = _nums;
-  }
 
-  function Increment() public{
-    nums++;
-  }
-  
-  function Decrement() public{
-    nums--;
+contract myToken is Erc20{
+
+  constructor(uint _initial_value){
+    //can mint initial tokens
   }
 }
